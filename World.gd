b136e4b6741f13de26a86613661f90df9c61563c
Tanishq Kundu <99400525+xXtanishq-524Xx@users.noreply.github.com>
2023-10-02 12:18:05 +0530

    Add files via upload

---
+++ World.gd
@@ -0,0 +1,80 @@
+extends Node2D
+
+onready var hud = $HUD
+onready var obstacle_spawner = $ObstacleSpawner
+
+const SAVE_FILE_PATH = "user://savedate.save"
+
+var score = 0 setget set_score
+var highscore = 0
+
+func _ready():
+	obstacle_spawner.connect("obstacle_created", self, "_on_obstacle_created")
+	load_highscore()
+	if highscore == null:
+		highscore = 0
+
+func _process(_delta):
+	if Input.is_action_pressed("reset"):
+		reset_highscore()
+	if Input.is_action_just_released("mute"):
+		if $BackgroundMusic.stream_paused == false:
+			$BackgroundMusic.set_stream_paused(true)
+		elif $BackgroundMusic.stream_paused == true:
+			$BackgroundMusic.set_stream_paused(false)
+
+func new_game():
+	self.score = 0
+	obstacle_spawner.start()
+
+func game_over():
+	obstacle_spawner.stop()
+	$Ground/AnimationPlayer.stop()
+	get_tree().call_group("obstacles", "set_physics_process", false)
+	
+	if score > highscore:
+		highscore = score
+		save_highscore()
+	
+	$MenuLayer.init_game_over_menu(score, highscore)
+
+func _on_Player_died():
+	game_over()
+
+func _on_MenuLayer_start_game():
+	new_game()
+
+func player_score():
+	self.score += 1
+	
+func set_score(new_score):
+	score = new_score
+	hud.update_score(score)
+
+func _on_obstacle_created(obs):
+	obs.connect("score", self, "player_score")
+
+func _on_DeathZone_body_entered(body):
+	if body is Player:
+		if body.has_method("die"):
+			body.die()
+
+func save_highscore():
+	var save_data = File.new()
+	save_data.open(SAVE_FILE_PATH, File.WRITE)
+	save_data.store_var(highscore)
+	save_data.close()
+
+func load_highscore():
+	var save_data = File.new()
+	if save_data.file_exists(SAVE_FILE_PATH):
+		save_data.open(SAVE_FILE_PATH, File.READ)
+		highscore = save_data.get_var()
+		save_data.close()
+
+func reset_highscore():
+	highscore =  0
+	var save_data = File.new()
+	save_data.open(SAVE_FILE_PATH, File.WRITE)
+	save_data.store_var(highscore)
+	save_data.close()
